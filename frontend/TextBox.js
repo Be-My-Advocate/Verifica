11c46ea0802afc7b5ac2d0814ee71c44c05c3f30
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-10-27 10:12:42 -0700

    added fixes

--- frontend/TextBox.js
+++ frontend/TextBox.js
@@ -1,32 +1,37 @@
-import React from 'react'
-import {StyleSheet, Text, View, TextInput} from 'react-native'
-
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const styles = StyleSheet.create({
-  container:{
-    width:'80%',
+  container: {
+    width: '80%',
     margin: 8
   },
-  label:{
-    fontSize:16,
-    fontWeight:'500'
+  label: {
+    fontSize: 16,
+    fontWeight: '500'
   },
-  inputBox:{
-    borderColor:'#333',
-    borderWidth:1,
+  inputBox: {
+    borderColor: '#333',
+    borderWidth: 1,
     fontSize: 24,
-    padding:12,
-    borderRadius:8
+    padding: 12,
+    borderRadius: 8
   }
 });
 
-const TextBox = (props) => {
+const TextBox = props => {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{props.label}</Text>
-      <TextInput autoCompleteType={props.type} secureTextEntry={props.password} style={styles.inputBox} onChangeText={props.onChange}/>
+      <TextInput
+        value={props.value}
+        autoCompleteType={props.type}
+        secureTextEntry={props.password}
+        style={styles.inputBox}
+        onChangeText={props.onChange}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default TextBox
+export default TextBox;
